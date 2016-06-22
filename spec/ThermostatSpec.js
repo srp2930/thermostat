7a9c5d96b920e34803cd441a6463052eec636324
srp2930 <s.rimingtonpounder@gmail.com>
2016-06-22 17:00:55 +0100

    thermostat logic completed

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,41 +1,73 @@
 describe('Thermostat', function() {
 
   var thermostat;
-  
+
   beforeEach(function() {
     thermostat = new Thermostat();
   });
-  
+
   it('has default temperature of 20', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.temperature()).toEqual(20);
   });
-  
+
   it('has power saving mode on by default', function(){
-    expect(thermostat.powersaving).toEqual(true);
+    expect(thermostat.powersaving()).toEqual(true);
   })
-  
+
   it('can increase the temperature', function() {
     thermostat.increment()
-    expect(thermostat.temperature).toEqual(21);
+    expect(thermostat.temperature()).toEqual(21);
   });
-  
+
   it('can decrease the temperature', function() {
     thermostat.decrease()
-    expect(thermostat.temperature).toEqual(19);
+    expect(thermostat.temperature()).toEqual(19);
   });
-  
+
   it('cannot decrease below 10', function() {
-   for(i=0;i<20;i++) {
+    for(i=0;i<20;i++) {
       thermostat.decrease();
     };
-  expect(thermostat.temperature).toEqual(10);
+    expect(thermostat.temperature()).toEqual(10);
   });
-  
-//    it('cannot increase above 25 (in power saving mode)', function() {
-//   for(i=0;i<20;i++) {
-//      thermostat.increment();
-//    };
-//    expect(thermostat.temperature).toEqual(25);
-//  });
-});
 
+  it('cannot increase above 25 (in power saving mode)', function() {
+    for(i=0;i<20;i++) {
+      thermostat.increment();
+    };
+    expect(thermostat.temperature()).toEqual(25);
+  });
+
+  it('can be taken off power saving mode', function () {
+    thermostat.setPoweringSaving(false);
+    expect(thermostat.powersaving()).toEqual(false);
+  });
+
+  it('can increase up to 32 if power saving mode is off', function() {
+    thermostat.setPoweringSaving(false);
+    for(i=0;i<20;i++) {
+      thermostat.increment();
+    };
+    expect(thermostat.temperature()).toEqual(32);
+  });
+
+  it('resets the thermostat', function() {
+    thermostat.reset();
+    expect(thermostat.temperature()).toEqual(20);
+  });
+
+  it('energy usage is high when temperature is greater than 25', function() {
+    thermostat.reset(26);
+    expect(thermostat.energyUsage()).toEqual('high');
+  });
+
+  it('energy usage is medium when temperaturs is between 18 and 25', function() {
+    thermostat.reset(18);
+    expect(thermostat.energyUsage()).toEqual('medium');
+  });
+
+  it('energy usage is low when temperature is lower than 18', function() {
+    thermostat.reset(10);
+    expect(thermostat.energyUsage()).toEqual('low');
+  });
+});
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,16 +1,54 @@
 function Thermostat() {
-  this.temperature = 20
-  this.powersaving = true
+  this._defaultTemperature = 20
+  this._temperature = this._defaultTemperature
+  this._powersaving = true
+  this._minTemperature = 10
+  this._maxTempPowerSavingOn = 25
+  this._maxTempPowerSavingOff = 32
 };
 
 Thermostat.prototype = {
+
+  temperature: function() {
+    return this._temperature;
+  },
+
+  powersaving: function() {
+    return this._powersaving;
+  },
+
   increment: function() {
-    this.temperature++;
+    if (this.powersaving()) {
+      var maxTemp = this._maxTempPowerSavingOn;
+    } else {
+      var maxTemp = this._maxTempPowerSavingOff;
+    };
+    if (this._temperature < maxTemp){
+      this._temperature++;
+    };
   },
 
   decrease: function() {
-    if (this.temperature > 10){
-      this.temperature--;
+    if (this._temperature > this._minTemperature){
+      this._temperature--;
     };
-  }
+  },
+
+  setPoweringSaving: function(onOrOff){
+    this._powersaving = onOrOff;
+  },
+
+  reset: function(value = this._defaultTemperature) {
+    this._temperature = value;
+  },
+
+  energyUsage: function() {
+    if (this.temperature() > 25) {
+      return 'high';
+    } else if(this.temperature() < 18 ) {
+      return 'low';
+    } else {
+      return 'medium';
+    };
+  },
 };
